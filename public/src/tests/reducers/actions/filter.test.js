5a02c236f2dc709d0c573ecfd015b9ab7215bfe9
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-08-12 16:05:44 -0500

    jest.config.json

---
+++ public/src/tests/reducers/actions/filter.test.js
@@ -0,0 +1,9 @@
+import {filtrarTexto,filtrarPorFecha,filtrarPorMonto,cambiarFechaFin,cambiarFechaInicio} from '../../../reducer/actions/filtro'
+
+
+describe('Test chido', () => {
+  test('should ', () => {
+  
+  })
+  
+})
