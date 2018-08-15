c1e456e8a7ac297ce806af1e0df76d373b08f11b
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-08-14 20:27:19 -0500

    test de totales

--- public/src/components/listaExpense.js
+++ public/src/components/listaExpense.js
@@ -5,8 +5,11 @@ import { TodosVisibles } from "./../reducer/selectors/visibles";
 import { eliminarTodo } from "./../reducer/actions/todos";
 import { Expense } from "./Expense";
 import { dispatch } from "rxjs/internal/observable/range";
+import SumaTotal from "./totalExpenses";
+
 export const ListaExpenses = ({ todos, func }) => (
   <div>
+    <SumaTotal />
     <h1>Expenses</h1>
     {todos ? (
       <ul>
